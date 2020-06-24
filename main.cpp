59df379222738d0353fbab826c98b676c8577098
Dennis Jimenez <alejimenezc@hotmail.com>
2020-06-23 19:54:12 -0600

    md5(sin adaptar)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
-#include <new>
-#include "GarbageCollector.h"
-
+#include "md5.h"
+#include "md5.cpp"
 using namespace std;
+using std::cin;
+using std::cout; 
+using std::endl;
 
-int main() {
+int main(int argc, char *argv[])
+{
+    string password;
+    cout << "Digite password: ";
+    cin >> password;
+    cout << "md5 of " + password + " is:" << md5(password) << endl;
+    return 0;
+}
+
+
+//#include <iostream>
+//#include <new>
+//#include "GarbageCollector.h"
+
+//using namespace std;
+
+
+
+/*int main() {
 
     GCPointer<int> p;
     
@@ -19,4 +39,4 @@ int main() {
     int k = *p;
     cout << "k is " << k << endl;
     return 0;
-}
+}*/
