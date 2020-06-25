be4143939aaef8a5b84e959c634593459cc3df93
Dennis Jimenez <alejimenezc@hotmail.com>
2020-06-25 14:00:22 -0600

    Prueba de conexion con el server

--- client.c
+++ client.c
@@ -4,12 +4,11 @@
 #include <unistd.h> 
 #include <string.h> 
 #define PORT 8080 
-   
 int main(int argc, char const *argv[]) 
 { 
     int sock = 0, valread; 
     struct sockaddr_in serv_addr; 
-    char *hello = "Hello from client"; 
+    char *hello = "Conexion exitosa"; 
     char buffer[1024] = {0}; 
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
     { 
@@ -20,7 +19,7 @@ int main(int argc, char const *argv[])
     serv_addr.sin_family = AF_INET; 
     serv_addr.sin_port = htons(PORT); 
        
-    // Convert IPv4 and IPv6 addresses from text to binary form 
+    // Convierte IPv4 e IPv6 (Direcciones) de texto a binario
     if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
     { 
         printf("\nInvalid address/ Address not supported \n"); 
@@ -33,7 +32,7 @@ int main(int argc, char const *argv[])
         return -1; 
     } 
     send(sock , hello , strlen(hello) , 0 ); 
-    printf("Hello message sent\n"); 
+    printf("Conexion encontrada con server\n"); 
     valread = read( sock , buffer, 1024); 
     printf("%s\n",buffer ); 
     return 0; 
--- server.c
+++ server.c
@@ -1,5 +1,4 @@
 
-// Server side C/C++ program to demonstrate Socket programming 
 #include <unistd.h> 
 #include <stdio.h> 
 #include <sys/socket.h> 
@@ -7,6 +6,7 @@
 #include <netinet/in.h> 
 #include <string.h> 
 #define PORT 8080 
+
 int main(int argc, char const *argv[]) 
 { 
     int server_fd, new_socket, valread; 
@@ -14,16 +14,16 @@ int main(int argc, char const *argv[])
     int opt = 1; 
     int addrlen = sizeof(address); 
     char buffer[1024] = {0}; 
-    char *hello = "Hello from server"; 
+    char *hello = "Conexion exitosa"; 
        
-    // Creating socket file descriptor 
+    // Creando descriptor del socket del server 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
     { 
         perror("socket failed"); 
         exit(EXIT_FAILURE); 
     } 
        
-    // Forcefully attaching socket to the port 8080 
+    // Se asigna al puerto 8080 a la fuerza
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
                                                   &opt, sizeof(opt))) 
     { 
@@ -33,8 +33,6 @@ int main(int argc, char const *argv[])
     address.sin_family = AF_INET; 
     address.sin_addr.s_addr = INADDR_ANY; 
     address.sin_port = htons( PORT ); 
-       
-    // Forcefully attaching socket to the port 8080 
     if (bind(server_fd, (struct sockaddr *)&address,  
                                  sizeof(address))<0) 
     { 
@@ -55,6 +53,6 @@ int main(int argc, char const *argv[])
     valread = read( new_socket , buffer, 1024); 
     printf("%s\n",buffer ); 
     send(new_socket , hello , strlen(hello) , 0 ); 
-    printf("Hello message sent\n"); 
+    printf("Conexion encontrada con cliente\n"); 
     return 0; 
 } 
